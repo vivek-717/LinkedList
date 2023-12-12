83e5a13851a392b45ba710221154bbf652b92317
Vivek Prasannan <prasannanvivek@gmail.com>
2023-12-12 06:49:24 +0530

    DLL added

---
+++ src/SinglyLinkedList/DLL.java
@@ -0,0 +1,155 @@
+package SinglyLinkedList;
+
+public class DLL {
+    private Node head;
+
+    public DLL(){
+        this.head = null;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        if (head!=null){
+            head.prev = node;
+        }
+        node.prev = null;
+        head = node;
+    }
+
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+        if (head==null){
+            insertFirst(val);
+            return;
+        }
+        while(last.next!=null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+        node.next = null;
+    }
+
+    public void insert(int val, int index){
+        if (index==0){
+            insertFirst(val);
+            return;
+        }
+        Node temp = head;
+        for (int i=1; i<index; i++){
+            temp =temp.next;
+        }
+        Node node = new Node(val);
+        node.next = temp.next;
+        temp.next = node;
+        node.prev = temp;
+        if (node.next!=null){
+            node.next.prev = node;
+        }
+    }
+
+    public void insertAfter(int val, int after){
+        Node temp = get(after);
+        if (temp==null){
+            System.out.println("cannot be inserted");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = temp.next;
+        temp.next = node;
+        node.prev = temp;
+        if (node.next!=null){
+            node.next.prev = node;
+        }
+    }
+
+    public int deleteFirst(){
+        int value = head.value;
+        if (head!=null){
+            head = head.next;
+        }
+        return value;
+    }
+
+    public int deleteLast(){
+        Node temp = head;
+        while (temp.next!=null){
+            temp = temp.next;
+        }
+        int value = temp.value;
+        temp.prev.next = null;
+        return value;
+    }
+
+    public int delete(int value){
+        Node node = get(value);
+        if (node==null){
+            System.out.println("No such value to delete");
+            return -1;
+        }
+        if (node == head){
+            return deleteFirst();
+        }
+        Node temp = head;
+        while (temp.next!=node){
+            temp=temp.next;
+        }
+        int val = node.value;
+        temp.next = node.next;
+        if (node.next!=null){
+            node.next.prev = temp;
+        }
+        return val;
+
+
+    }
+    public Node get(int value){
+        Node temp = head;
+        while (head!=null && temp.next!=null){
+            if (temp.value==value){
+                return temp;
+            }
+            temp=temp.next;
+
+        }
+        return null;
+    }
+    public void display(){
+        Node node = head;
+        Node last = null;
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("Print in Reverse");
+        while(last!=head){
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println(head.value + " -> START");
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node (int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+
+}
+
--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -4,20 +4,38 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        DLL list = new DLL();
+//        list.insertFirst(2);
+//        list.insertFirst(5);
+//        list.insertFirst(18);
+//        list.insertFirst(20);
+//        list.insertFirst(23);
+//        list.display();
+//        list.insert(40,2);
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        list.delete(2);
+//        list.display();
+//        list.find (18);
         list.insertFirst(2);
         list.insertFirst(5);
         list.insertFirst(18);
         list.insertFirst(20);
         list.insertFirst(23);
+        list.insertLast(50);
+        list.insertLast(30);
+        list.insertLast(15);
         list.display();
-        list.insert(40,2);
+////        list.insertAfter(12,11);
+////        list.display();
+//        list.deleteFirst();
+//        list.display();
+//        list.deleteFirst();
+//        list.deleteLast();
+//        list.display();
+        System.out.println(list.delete(50));
         list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        list.delete(2);
-        list.display();
-        list.find (18);
 
 
     }
