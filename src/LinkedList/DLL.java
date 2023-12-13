60ef82ae94a34df9f8eea305ba1403d5fe4b9df9
Vivek Prasannan <prasannanvivek@gmail.com>
2023-12-13 10:34:09 +0530

    Insertion and Deletion in DLL

--- src/LinkedList/DLL.java
+++ src/LinkedList/DLL.java
@@ -7,148 +7,200 @@ public class DLL {
         this.head = null;
     }
 
-    public void insertFirst(int val){
-        Node node = new Node(val);
-        node.next = head;
-        if (head!=null){
-            head.prev = node;
-        }
-        node.prev = null;
-        head = node;
-    }
-
-    public void insertLast(int val){
-        Node node = new Node(val);
-        Node last = head;
-        if (head==null){
-            insertFirst(val);
-            return;
-        }
-        while(last.next!=null){
-            last = last.next;
-        }
-        last.next = node;
-        node.prev = last;
-        node.next = null;
-    }
-
-    public void insert(int val, int index){
-        if (index==0){
-            insertFirst(val);
-            return;
-        }
-        Node temp = head;
-        for (int i=1; i<index; i++){
-            temp =temp.next;
-        }
-        Node node = new Node(val);
-        node.next = temp.next;
-        temp.next = node;
-        node.prev = temp;
-        if (node.next!=null){
-            node.next.prev = node;
-        }
-    }
-
-    public void insertAfter(int val, int after){
-        Node temp = get(after);
-        if (temp==null){
-            System.out.println("cannot be inserted");
-            return;
-        }
-        Node node = new Node(val);
-        node.next = temp.next;
-        temp.next = node;
-        node.prev = temp;
-        if (node.next!=null){
-            node.next.prev = node;
-        }
-    }
-
-    public int deleteFirst(){
-        int value = head.value;
-        if (head!=null){
-            head = head.next;
-        }
-        return value;
-    }
-
-    public int deleteLast(){
-        Node temp = head;
-        while (temp.next!=null){
-            temp = temp.next;
-        }
-        int value = temp.value;
-        temp.prev.next = null;
-        return value;
-    }
-
-    public int delete(int value){
-        Node node = get(value);
-        if (node==null){
-            System.out.println("No such value to delete");
-            return -1;
-        }
-        if (node == head){
-            return deleteFirst();
-        }
-        Node temp = head;
-        while (temp.next!=node){
-            temp=temp.next;
-        }
-        int val = node.value;
-        temp.next = node.next;
-        if (node.next!=null){
-            node.next.prev = temp;
-        }
-        return val;
-
-
-    }
-    public Node get(int value){
-        Node temp = head;
-        while (head!=null && temp.next!=null){
-            if (temp.value==value){
-                return temp;
-            }
-            temp=temp.next;
-
-        }
-        return null;
-    }
-    public void display(){
-        Node node = head;
-        Node last = null;
-        while (node != null){
-            System.out.print(node.value + " -> ");
-            last = node;
-            node = node.next;
-        }
-        System.out.println("END");
-
-        System.out.println("Print in Reverse");
-        while(last!=head){
-            System.out.print(last.value + " -> ");
-            last = last.prev;
-        }
-        System.out.println(head.value + " -> START");
-    }
-
     private class Node {
         private int value;
         private Node next;
         private Node prev;
-
-        public Node (int value){
-            this.value = value;
-        }
 
         public Node(int value, Node next, Node prev) {
             this.value = value;
             this.next = next;
             this.prev = prev;
         }
+
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
     }
+
+    public void insertFirst(int val){
+            Node node = new Node(val);
+            if (head == null){
+                head = node;
+                return;
+            }
+            node.next = head;
+            head.prev = node;
+            head = node;
+    }
+
+    public void insertLast(int val){
+        if (head == null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        Node temp = head;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next = node;
+        node.prev = temp;
+        node.next = null;
+    }
+
+    public void insert (int val, int index){
+        Node temp = head;
+        if (temp==null){
+            insertFirst(val);
+            return;
+        }
+        int size = size(head);
+        if (index>size){
+            System.out.println("Index out of bound");
+            return;
+        }
+        if (index==0){
+            insertFirst(val);
+            return;
+        }
+        if (index==size-1){
+            insertLast(val);
+            return;
+        }
+        Node node = new Node(val);
+        for (int i=1; i<index; i++){
+            temp=temp.next;
+        }
+        node.next = temp.next;
+        temp.next.prev = node;
+        temp.next = node;
+        node.prev = temp;
+
+    }
+
+    public void insertEle(int val, int after){
+        Node temp = head;
+        if (temp==null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        while(temp.next!=null){
+            if (temp.value == after){
+                node.next = temp.next;
+                temp.next.prev = node;
+                node.prev=temp;
+                temp.next = node;
+                return;
+            }
+            temp= temp.next;
+        }
+        if (temp.value==after){
+            insertLast(val);
+            return;
+        }
+        System.out.println("Given element not present in DLL");
+    }
+
+    public int deleteFirst(){
+        if (head==null){
+            System.out.println("No elements in DLL to delete");
+            return -1;
+        }
+        int value = head.value;
+        head = head.next;
+        head.prev = null;
+        return value;
+    }
+
+    public int deleteLast(){
+        if (head==null){
+            System.out.println("No elements in DLL to delete");
+            return -1;
+        }
+        Node temp = head;
+        Node prev = null;
+        while (temp.next!=null){
+            prev = temp;
+            temp=temp.next;
+        }
+        int value = temp.value;
+        prev.next = null;
+        return value;
+    }
+
+    public int delete (int index){
+        if (index==0){
+            return deleteFirst();
+        }
+        int size = size(head);
+        if (index == size-1){
+            return deleteLast();
+        }
+        if (index>=size){
+            System.out.println("Index Out of Bound");
+            return -1;
+        }
+        Node temp = head;
+        Node prev = null;
+        for (int i=1; i<=index; i++){
+            prev = temp;
+            temp = temp.next;
+        }
+        int value = temp.value;
+        prev.next = temp.next;
+        temp.next.prev = prev;
+        return value;
+    }
+
+    public int deleteEle (int after){
+        if (head == null){
+            System.out.println("No element in DLL to delete");
+            return -1;
+        }
+        Node temp = head;
+        while (temp.next!=null && temp.next.next!=null){
+            if (temp.value==after){
+                int value = temp.next.value;
+                temp.next =temp.next.next;
+                temp.next.next.prev = temp;
+                return value;
+            }
+            temp = temp.next;
+        }
+        if (temp.value == after){
+            return deleteLast();
+        }
+        System.out.println("Element cannot be deleted");
+        return -1;
+
+    }
+
+    public int size (Node head){
+        Node temp = head;
+        if (temp == null){
+            return 0;
+        }
+        int count = 1;
+        while (temp.next!=null){
+            temp=temp.next;
+            count++;
+        }
+        return count;
+    }
+
+    public void display(){
+            Node temp = head;
+            while (temp!=null){
+                System.out.print(temp.value + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("END");
+    }
+
 
 
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,7 +2,7 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        DLL list = new DLL();
         list.insertFirst(5);
         list.insertFirst(10);
         list.insertFirst(15);
@@ -16,14 +16,19 @@ public class Main {
         list.insertLast(72);
         list.insertLast(21);
         list.insertLast(92);
-        list.display();
+//        list.display();
 //        list.insertEle(23,0);
 //        list.display();
 //        list.deleteLast();
 //        list.display();
-        System.out.println(list.deleteEle(21));
+//        System.out.println(list.deleteEle(21));
+//        list.display();
+//        System.out.println(list.deleteLast());
         list.display();
-
+//        list.insertEle(23,6);
+//        DLL list2 = new DLL();
+        System.out.println(list.deleteEle(92));
+        list.display();
 //        LL list1 = new LL();
 //        list1.deleteLast();
 
